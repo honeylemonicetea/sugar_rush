596969c1f7331cd2004f96c72f0c0dd200c59929
easydush <izidacasillas@mail.ru>
2020-07-18 02:26:19 +0300

    added some changes and advices

--- coffe_machine.py
+++ coffe_machine.py
@@ -1,14 +1,17 @@
-class CoffeeMachine():
-    water = 400
-    milk = 540
-    beans = 120
-    cups = 9
-    money = 550
+class CoffeeMachine:
+    # isis: todo error catching for unexpected arguments
+    # isis: added constructor
+    def __init__(self, water, milk, beans, cups, money):
+        self.water = water
+        self.milk = milk
+        self.beans = beans
+        self.cups = cups
+        self.money = money
+
     def get_action(self, action):
         self.action = action
         if self.action == 'buy':
-            print()
-            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+            print("\n What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
             self.action_buy(input())
         elif self.action == 'fill':
             self.action_fill()
@@ -18,100 +21,102 @@ class CoffeeMachine():
             self.action_remaining()
         elif action == 'exit':
             self.action_exit()
+
     def action_buy(self, coffee):
-        self.coffee = coffee
-        if self.coffee == '1':
-            if CoffeeMachine.water < 250:
+        if coffee == '1':
+            if self.water < 250:
                 print('Sorry, not enough water!')
-            elif CoffeeMachine.beans < 16:
+            elif self.beans < 16:
                 print('Sorry, not enough beans!')
-            elif CoffeeMachine.cups < 1:
+            elif self.cups < 1:
                 print('Sorry, not enough cups!')
-            elif CoffeeMachine.water >= 250 and CoffeeMachine.beans >= 16 and CoffeeMachine.cups >= 1:
+            elif self.water >= 250 and self.beans >= 16 and self.cups >= 1:
                 print("I have enough resources, making you a coffee!")
-                CoffeeMachine.water -= 250
-                CoffeeMachine.beans -= 16
-                CoffeeMachine.cups -= 1
-                CoffeeMachine.money += 4
-            print()
-            print("Write action(buy, fill, take, remaining, exit):")
+                self.water -= 250
+                self.beans -= 16
+                self.cups -= 1
+                self.money += 4
+            print("\n Write action(buy, fill, take, remaining, exit):")
             self.get_action(input())
-        elif self.coffee == '2':
-            if CoffeeMachine.water < 350:
+        elif coffee == '2':
+            if self.water < 350:
                 print('Sorry, not enough water!')
-            elif CoffeeMachine.milk < 75:
+            elif self.milk < 75:
                 print('Sorry, not enough milk!')
-            elif CoffeeMachine.beans < 20:
+            elif self.beans < 20:
                 print('Sorry, not enough beans!')
-            elif CoffeeMachine.cups < 1:
+            elif self.cups < 1:
                 print('Sorry, not enough cups!')
-            elif CoffeeMachine.water >= 350 and  CoffeeMachine.milk >=75 and  CoffeeMachine.beans >= 20 and  CoffeeMachine.cups >= 1:
+            elif self.water >= 350 and self.milk >= 75 and self.beans >= 20 and self.cups >= 1:
                 print("I have enough resources, making you a coffee!")
-                CoffeeMachine.water -= 350
-                CoffeeMachine.milk -= 75
-                CoffeeMachine.beans -= 20
-                CoffeeMachine.cups -= 1
-                CoffeeMachine.money += 7
+                self.water -= 350
+                self.milk -= 75
+                self.beans -= 20
+                self.cups -= 1
+                self.money += 7
             print()
             print("Write action(buy, fill, take, remaining, exit):")
             self.get_action(input())
-        elif self.coffee == '3':
-            if CoffeeMachine.water < 200:
+        elif coffee == '3':
+            if self.water < 200:
                 print('Sorry, not enough water!')
-            elif CoffeeMachine.milk < 100:
+            elif self.milk < 100:
                 print('Sorry, not enough milk!')
-            elif CoffeeMachine.beans < 12:
+            elif self.beans < 12:
                 print('Sorry, not enough beans!')
-            elif CoffeeMachine.cups < 1:
+            elif self.cups < 1:
                 print('Sorry, not enough cups!')
-            elif CoffeeMachine.water >= 200 and CoffeeMachine.milk >=100 and CoffeeMachine.beans >= 12 and CoffeeMachine.cups >= 1:
+            elif self.water >= 200 and self.milk >= 100 and self.beans >= 12 and self.cups >= 1:
                 print("I have enough resources, making you a coffee!")
-                CoffeeMachine.water -= 200
-                CoffeeMachine.milk -= 100
-                CoffeeMachine.beans -= 12
-                CoffeeMachine.cups -= 1
-                CoffeeMachine.money += 6
-            print()
-            print("Write action(buy, fill, take, remaining, exit):")
+                self.water -= 200
+                self.milk -= 100
+                self.beans -= 12
+                self.cups -= 1
+                self.money += 6
+            print("\n Write action(buy, fill, take, remaining, exit):")
             self.get_action(input())
-        elif self.coffee == 'back':
-            print()
-            print("Write action(buy, fill, take, remaining, exit):")
+        elif coffee == 'back':
+            print("\n Write action(buy, fill, take, remaining, exit):")
             self.get_action(input())
+
     def action_fill(self):
-        print()
-        print("Write how many ml of water do you want to add:")
+        print("\n Write how many ml of water do you want to add:")
         a_water = int(input())
-        CoffeeMachine.water = CoffeeMachine.water + a_water
+        self.water = self.water + a_water
         print("Write how many ml of milk do you want to add:")
         a_milk = int(input())
-        CoffeeMachine.milk = CoffeeMachine.milk + a_milk
+        self.milk = self.milk + a_milk
         print("Write how many grams of coffee beans do you want to add:")
         a_beans = int(input())
-        CoffeeMachine.beans = a_beans + CoffeeMachine.beans
+        self.beans = a_beans + self.beans
         print("Write how many disposable cups of coffee do you want to add:")
         a_cups = int(input())
-        CoffeeMachine.cups = a_cups + CoffeeMachine.cups
+        self.cups = a_cups + self.cups
         print("Write action(buy, fill, take, remaining, exit):")
         self.get_action(input())
+
     def action_take(self):
-        print("I gave you {}".format(CoffeeMachine.money))
-        CoffeeMachine.money = 0
-        print()
-        print("Write action(buy, fill, take, remaining, exit):")
+        print("I gave you {}".format(self.money))
+        self.money = 0
+        print("\n Write action(buy, fill, take, remaining, exit):")
         self.get_action(input())
+
     def action_remaining(self):
         print("The coffee machine has:")
-        print(CoffeeMachine.water, 'of water')
-        print(CoffeeMachine.milk, "of milk")
-        print(CoffeeMachine.beans, "of coffee beans")
-        print(CoffeeMachine.cups, "of disposable cups")
-        print("$ {} of money".format(CoffeeMachine.money))
-        print()
-        print("Write action(buy, fill, take, remaining, exit):")
+        print(f'{self.water} of water')
+        # isis: use this!
+        print(self.milk, "of milk")
+        print(self.beans, "of coffee beans")
+        print(self.cups, "of disposable cups")
+        # isis: the method upper is more simple
+        print("$ {} of money".format(self.money))
+        print("\n Write action(buy, fill, take, remaining, exit):")
         self.get_action(input())
+
     def action_exit(self):
         return 0
-a = CoffeeMachine()
+
+
+a = CoffeeMachine(400, 540, 120, 9, 550)
 print("Write action(buy, fill, take, remaining, exit):")
 a.get_action(input())
