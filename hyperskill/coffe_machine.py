e4556268bcab8a73c13f95afeb157f965324afc1
Bella <107717156+mistymisthios@users.noreply.github.com>
2022-07-31 23:48:50 +0300

    Update coffe_machine.py

--- hyperskill/coffe_machine.py
+++ hyperskill/coffe_machine.py
@@ -8,12 +8,12 @@ class CoffeeMachine:
         self.cups = cups
         self.money = money
 
-    def get_action(self, action):
-        self.action = action
-        if self.action == 'buy':
-            print("\n What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-            self.action_buy(input())
-        elif self.action == 'fill':
+    # why create an attribute if you can just use the parameter?
+    def get_action(self):
+        action = input('\n Write action(buy, fill, take, remaining, exit):')
+        if action == 'buy':
+            self.action_buy()
+        elif action == 'fill':
             self.action_fill()
         elif action == 'take':
             self.action_take()
@@ -22,7 +22,8 @@ class CoffeeMachine:
         elif action == 'exit':
             self.action_exit()
 
-    def action_buy(self, coffee):
+    def action_buy(self):
+        coffee = input("\n What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
         if coffee == '1':
             if self.water < 250:
                 print('Sorry, not enough water!')
@@ -36,8 +37,7 @@ class CoffeeMachine:
                 self.beans -= 16
                 self.cups -= 1
                 self.money += 4
-            print("\n Write action(buy, fill, take, remaining, exit):")
-            self.get_action(input())
+            self.get_action()
         elif coffee == '2':
             if self.water < 350:
                 print('Sorry, not enough water!')
@@ -54,9 +54,7 @@ class CoffeeMachine:
                 self.beans -= 20
                 self.cups -= 1
                 self.money += 7
-            print()
-            print("Write action(buy, fill, take, remaining, exit):")
-            self.get_action(input())
+            self.get_action()
         elif coffee == '3':
             if self.water < 200:
                 print('Sorry, not enough water!')
@@ -73,50 +71,36 @@ class CoffeeMachine:
                 self.beans -= 12
                 self.cups -= 1
                 self.money += 6
-            print("\n Write action(buy, fill, take, remaining, exit):")
-            self.get_action(input())
+            self.get_action()
         elif coffee == 'back':
-            print("\n Write action(buy, fill, take, remaining, exit):")
-            self.get_action(input())
+            self.get_action()
 
     def action_fill(self):
-        print("\n Write how many ml of water do you want to add:")
-        a_water = int(input())
-        self.water = self.water + a_water
-        print("Write how many ml of milk do you want to add:")
-        a_milk = int(input())
-        self.milk = self.milk + a_milk
-        print("Write how many grams of coffee beans do you want to add:")
-        a_beans = int(input())
-        self.beans = a_beans + self.beans
-        print("Write how many disposable cups of coffee do you want to add:")
-        a_cups = int(input())
-        self.cups = a_cups + self.cups
-        print("Write action(buy, fill, take, remaining, exit):")
-        self.get_action(input())
+        a_water = int(input("\n Write how many ml of water do you want to add:"))
+        self.water += a_water
+        a_milk = int(input("Write how many ml of milk do you want to add:"))
+        self.milk += a_milk
+        a_beans = int(input("Write how many grams of coffee beans do you want to add:"))
+        self.beans += a_beans
+        a_cups = int(input("Write how many disposable cups of coffee do you want to add:"))
+        self.cups += a_cups
+        self.get_action()
 
     def action_take(self):
-        print("I gave you {}".format(self.money))
+        print(f"I gave you {self.money}")
         self.money = 0
-        print("\n Write action(buy, fill, take, remaining, exit):")
-        self.get_action(input())
+        self.get_action()
 
     def action_remaining(self):
         print("The coffee machine has:")
-        print(f'{self.water} of water')
-        # isis: use this!
-        print(self.milk, "of milk")
-        print(self.beans, "of coffee beans")
-        print(self.cups, "of disposable cups")
+        print(f'\n{self.water} of water\n{self.milk} of milk\n{self.beans} of coffee beans\n{self.cups} of disposable cups')
         # isis: the method upper is more simple
-        print("$ {} of money".format(self.money))
-        print("\n Write action(buy, fill, take, remaining, exit):")
-        self.get_action(input())
+        print(f"{self.money} of money")
+        self.get_action()
 
     def action_exit(self):
         return 0
 
 
 a = CoffeeMachine(400, 540, 120, 9, 550)
-print("Write action(buy, fill, take, remaining, exit):")
-a.get_action(input())
+a.get_action()
